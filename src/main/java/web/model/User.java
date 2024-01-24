b907f146338fc95b13bb8ee1453fd134c12098a2
exs777 <exs777@mail.ru>
2024-01-24 18:57:08 +0300

    commit2

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -16,12 +16,35 @@ private String name;
 @Column(name = "surname")
 private String surname;
 
+@Column(name = "age")
+private int age;
+@Column(name = "email")
+private String email;
+
 public User() {
 }
 
-public User(String name, String surname) {
+public User(String name, String surname, int age, String email) {
     this.name = name;
     this.surname = surname;
+    this.age = age;
+    this.email = email;
+}
+
+public int getAge() {
+    return age;
+}
+
+public void setAge(int age) {
+    this.age = age;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
 }
 
 public int getId() {
